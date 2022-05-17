3c856b8256146a83e24e8fce40ac8e422ee57d7d
Joshua Hasulchan Kim <joshuah.kim@Jefes-iPhone.localdomain>
2022-05-16 22:28:39 -0400

    twoo new pages

---
+++ pages/work.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function WorkPage() {
+    return (
+      <>
+        <h1>Work</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </>
+    )
+  }
